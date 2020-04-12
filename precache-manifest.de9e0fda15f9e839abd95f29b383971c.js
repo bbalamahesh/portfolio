bb2b1ca0df688d1ba8df152f7986b4f11c6b35b0
Bala Mahesh B <balamahesh_ba@CHMB104962.local>
2020-04-12 22:10:28 +0530

    git hub updates

---
+++ precache-manifest.de9e0fda15f9e839abd95f29b383971c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f89a1f108883c6556419b0a1a95b4585",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f102f288d10231efb20",
+    "url": "/static/css/main.972abdb3.chunk.css"
+  },
+  {
+    "revision": "e1614e75ef41c633c06f",
+    "url": "/static/js/2.29184370.chunk.js"
+  },
+  {
+    "revision": "84f48c9a090ba2f8cc4a11e48dd3dad0",
+    "url": "/static/js/2.29184370.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f102f288d10231efb20",
+    "url": "/static/js/main.045af067.chunk.js"
+  },
+  {
+    "revision": "89a9cacf7c0736767db7",
+    "url": "/static/js/runtime-main.11cf4071.js"
+  },
+  {
+    "revision": "988c2d0652e4ff3df8e57e8e1851ef29",
+    "url": "/static/media/avatar-01.988c2d06.png"
+  },
+  {
+    "revision": "87df80cfd8833b871942912777939bf2",
+    "url": "/static/media/bg-03.87df80cf.jpg"
+  },
+  {
+    "revision": "3d0a856c6a4dfc44e8300dc21e9d9755",
+    "url": "/static/media/proj01.3d0a856c.jpg"
+  },
+  {
+    "revision": "93b8e634d6e80d5d5e4cbe75267ca8db",
+    "url": "/static/media/proj02.93b8e634.jpg"
+  },
+  {
+    "revision": "e779fb1df479f73b638a71e0b1a38ef3",
+    "url": "/static/media/proj03.e779fb1d.jpg"
+  },
+  {
+    "revision": "2d246720fe21f6f6e7df8128ebdcb40a",
+    "url": "/static/media/proj04.2d246720.png"
+  }
+]);
